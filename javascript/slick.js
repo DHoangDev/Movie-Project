03fc55536eadbd82ff8d94e0c250e00e73272dca
Hoang <hoangntu56@gmail.com>
2021-05-30 19:13:25 +0700

    chnaged

--- javascript/slick.js
+++ javascript/slick.js
@@ -1,6 +1,22 @@
-$('.carousel_slick').slick({
+$('.carousel_slick_1').slick({
     infinite: true,
     dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
+$('.carousel_slick_2').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
+$('.carousel_slick_3').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
+$('.carousel_slick_4').slick({
+    arrows: false,
+    infinite: true,
     autoplay: true,
     autoplaySpeed: 4000,
 });
