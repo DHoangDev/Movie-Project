cadd8adb5d6abf09d5957d85a9bb4edbb1c1354f
Hoang <hoangntu56@gmail.com>
2021-06-01 23:29:14 +0700

    ohho

--- javascript/slick.js
+++ javascript/slick.js
@@ -3,16 +3,19 @@ $('.carousel_slick_1').slick({
     dots: true,
     autoplay: true,
     autoplaySpeed: 4000,
+    slidesToShow: 1,
 });
 $('.carousel_slick_2').slick({
     infinite: true,
     autoplay: true,
     autoplaySpeed: 4000,
+    slidesToShow: 1,
 });
 $('.carousel_slick_3').slick({
     infinite: true,
     autoplay: true,
     autoplaySpeed: 4000,
+    slidesToShow: 1,
 });
 $('.carousel_slick_4').slick({
     arrows: false,
